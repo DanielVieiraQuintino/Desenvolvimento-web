aa304a0895a3bae677c1cf76ecc5f912b7518cf4
Daniel Quintino <danieljucas123@gmail.com>
2023-10-20 19:22:21 -0300

    update forEach map class

--- 4-JS/class-07-array-methods/forEach-map.js
+++ 4-JS/class-07-array-methods/forEach-map.js
@@ -22,3 +22,19 @@ const realConverted = real.map(value => value * 5.05);
 
 console.log(real);
 console.log(realConverted);
+
+const rectangles = [
+    { width: 4, height: 5 },
+    { width: 7, height: 10 },
+    { width: 14, height: 7 },
+]
+
+const calculateAreas = rectangles.map((value, index) =>{
+    return {
+    name: `Retangulo ${index + 1}`,
+    area: value.width * value.height
+    };
+});
+
+console.table(calculateAreas);
+document.write(JSON.stringify(calculateAreas))
