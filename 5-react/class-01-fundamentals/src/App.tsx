41e9f3b22968f08a3f58bb0e71ef91179dafb466
Daniel Quintino <danieljucas123@gmail.com>
2023-12-06 20:33:50 -0300

    add card profile

---
+++ 5-react/class-01-fundamentals/src/App.tsx
@@ -0,0 +1,13 @@
+import imageReact from "./assets/react.svg";
+
+// named
+export function App() {
+  return (
+    <>
+      <h1>Class 01 Fundamentals</h1>
+      <p>Syntex JSX</p>
+      <img src="vite.svg" alt="" />
+      <img src= {imageReact } alt="" />
+    </>
+  );
+}
---
+++ 5-react/class-02-components/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { CardProfile } from "../components/CardProfile";
+
+export function App() {
+  return (
+    <>
+    <h1>Class 02 Components</h1>
+    <CardProfile name={"Daniel"} age={30} />
+    <CardProfile name={"Eric"} age={19}/>
+    </>
+  );
+}
