bdc7d46ab9610f2daef3cb234f664776eeb203e2
Daniel Quintino <danieljucas123@gmail.com>
2023-12-07 20:51:53 -0300

    add class calcAge

--- 5-react/class-02-components/src/pages/App.tsx
+++ 5-react/class-02-components/src/pages/App.tsx
@@ -1,11 +1,26 @@
 import { CardProfile } from "../components/CardProfile";
 
+function calcAge(birth: string) {
+  const dateNow = new Date().getTime();
+  const dateBirth = new Date(birth).getTime();
+  const userAge = dateNow - dateBirth;
+  return Math.floor(userAge / (1000 * 60 * 60 * 24 * 365.25));
+}
+
+const dataAPI = [
+  { id: 1, name: "Daniel", birth: "05-26-2007" },
+  { id: 2, name: "Éric", birth: "06-18-2004" },
+  { id: 3, name: "Jorge", birth: "01-23-1995" },
+];
+
 export function App() {
   return (
     <>
-    <h1>Class 02 Components</h1>
-    <CardProfile name={"Daniel"} age={30} />
-    <CardProfile name={"Eric"} age={19}/>
+      <h1>Class 02 Components</h1>
+
+      {dataAPI.map(({ id, name, birth }) => {
+        return <CardProfile key={id} name={name} birth={birth} calcAge={calcAge}/>;
+      })}
     </>
   );
 }
