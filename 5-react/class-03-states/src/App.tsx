5a45699cee3557dd1266ea05b2f3d9bddc794e55
Daniel Quintino <danieljucas123@gmail.com>
2023-12-11 20:29:19 -0300

    add class

--- 5-react/class-03-states/src/App.tsx
+++ 5-react/class-03-states/src/App.tsx
@@ -1,30 +1,37 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 type UserData = {
   name: string;
   avatar_url: string;
   bio: string;
-}
+};
 
 export function App() {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("Daniel");
+  const [name, setName] = useState("DanielQuintino");
+  const [auxName, setAuxName] = useState("");
   const [data, setData] = useState<UserData>({} as UserData);
 
   function addCount() {
     // Closures
-    setCount((prevState) => prevState + 1);
+    // setCount((prevState) => prevState + 1);
+    setCount(count + 1);
   }
 
   function handleChangeName(event: ChangeEvent<HTMLInputElement>) {
     console.log(event?.target.value);
   }
 
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    setAuxName(name);
+  }
+
   useEffect(() => {
-    fetch(`https://api.github.com/users/${name}`)
+    fetch(`https://api.github.com/users/${auxName}`)
       .then((response) => response.json())
       .then((data) => setData(data));
-  }, [name]);
+  }, [auxName]);
 
   console.log(setName);
 
@@ -34,7 +41,13 @@ export function App() {
       <p>Name: {name}</p>
       <p>Count: {count}</p>
       <button onClick={addCount}>Add&gt;</button>
-      <input type="text" onChange={handleChangeName} />
+      <form
+        onSubmit={handleSubmit}
+        style={{ textAlign: "center", marginTop: 24 }}
+      >
+        <input type="text" onChange={handleChangeName} />
+        <button>Pesquisar</button>
+      </form>
       {data && (
         <section>
           <h2>{data.name}</h2>
