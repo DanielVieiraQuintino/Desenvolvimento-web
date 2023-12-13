aed0cc157c9932fa4befd9e18d924a9c066496d9
Daniel Quintino <danieljucas123@gmail.com>
2023-12-13 20:52:54 -0300

    add class css js

--- 5-react/class-04-contexts/src/components/Button.tsx
+++ 5-react/class-04-contexts/src/components/Button.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 export function Button() {
-  const { setName } = useContext(UserContext)
+  const { setName } = useContext(UserContext);
   return (
     <>
       <button onClick={() => setName("Quintino")}>Change name</button>
